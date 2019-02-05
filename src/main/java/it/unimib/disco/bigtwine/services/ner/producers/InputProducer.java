d11683219f176e63bca899360d85f43ac34566b3
Fausto <fausto91@gmail.com>
2019-02-05 22:29:16 +0100

    Pulizia import non usati

--- src/main/java/it/unimib/disco/bigtwine/services/ner/producers/InputProducer.java
+++ src/main/java/it/unimib/disco/bigtwine/services/ner/producers/InputProducer.java
@@ -3,11 +3,6 @@ package it.unimib.disco.bigtwine.services.ner.producers;
 import it.unimib.disco.bigtwine.commons.models.BasicTweet;
 import it.unimib.disco.bigtwine.commons.producers.GenericInputProducer;
 
-import java.io.Closeable;
-import java.io.Flushable;
-import java.io.IOException;
-import java.io.Writer;
-
 public interface InputProducer extends GenericInputProducer<BasicTweet> {
 
 }
