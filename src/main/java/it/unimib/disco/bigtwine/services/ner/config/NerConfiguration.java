8fab760fdaeeb483a065b645c068a22012aaf1d2
Fausto <fausto91@gmail.com>
2019-07-30 15:33:41 +0200

    Configurazione kafka template e percorsi predefiniti

--- src/main/java/it/unimib/disco/bigtwine/services/ner/config/NerConfiguration.java
+++ src/main/java/it/unimib/disco/bigtwine/services/ner/config/NerConfiguration.java
@@ -1,11 +1,14 @@
 package it.unimib.disco.bigtwine.services.ner.config;
 
+import it.unimib.disco.bigtwine.commons.messaging.KafkaUtils;
 import it.unimib.disco.bigtwine.services.ner.executors.ExecutorFactory;
 import it.unimib.disco.bigtwine.services.ner.parsers.OutputParserBuilder;
 import it.unimib.disco.bigtwine.services.ner.processors.ProcessorFactory;
 import it.unimib.disco.bigtwine.services.ner.producers.InputProducerBuilder;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,10 +23,15 @@ import java.util.Map;
 @Configuration
 public class NerConfiguration {
 
+    private final Logger log = LoggerFactory.getLogger(NerConfiguration.class);
+
     private ApplicationProperties appProps;
 
-    @Value("spring.cloud.stream.kafka.binder.brokers")
+    @Value("${spring.cloud.stream.kafka.binder.brokers}")
     private String kafkaBrokers;
+
+    @Value("${spring.cloud.stream.kafka.binder.defaultBrokerPort:9092}")
+    private Integer kafkaDefaultBrokerPort;
 
     public NerConfiguration(ApplicationProperties appProps) {
          this.appProps = appProps;
@@ -56,10 +64,15 @@ public class NerConfiguration {
 
     @Bean
     public Map<String, Object> producerConfigs() {
+        String bootstrapServers = KafkaUtils.buildBootstrapServersConfig(this.kafkaBrokers, this.kafkaDefaultBrokerPort);
+
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaBrokers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        log.debug("Configuring kafka with props: {}", props);
+
         // See https://kafka.apache.org/documentation/#producerconfigs for more properties
         return props;
     }
